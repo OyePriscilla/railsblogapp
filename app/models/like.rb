8abf2bea8d68810f7782ff87b5dd993052a41d72
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-10-20 09:51:22 +0100

    Add models for users,comments,posts and likes

---
+++ app/models/like.rb
@@ -0,0 +1,10 @@
+class Like < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  belongs_to :posts
+
+  after_save :likes_counter
+
+  def likes_counter
+    post.increment!(:likes_counter)
+  end
+end
---
+++ app/models/post.rb
@@ -0,0 +1,17 @@
+class Post < ApplicationRecord
+  has_many :comments
+  has_many :likes
+  belongs_to :author, class_name: 'User'
+
+  after_save :update_posts_counter
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
+  end
+end
