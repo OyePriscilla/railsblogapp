f115ab1240e547e894b7dc9b587d5b1706d869ec
Oyebade Priscilla <oyebadepriscilla22@gmail.com>
2022-11-02 16:32:55 +0100

    Remove current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :current_user
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def current_user
-    User.first
-  end
 end
